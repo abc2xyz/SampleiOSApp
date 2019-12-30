3c1b0bb668626ccb9899f6bae152aa7847f8b730
Shiv Kumar <shiv.meena@goniyo.com>
2019-12-30 18:08:57 +0530

    Initial Commit

---
+++ SampleiOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleiOSApp
+//
+//  Created by Shiv Kumar on 30/12/19.
+//  Copyright © 2019 Shiv Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
